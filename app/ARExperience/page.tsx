9498c170865489d075f401a0873d3567c9b51139
Yemen-Programer <83095258+Yemen-Programer@users.noreply.github.com>
2025-11-23 12:51:57 +0300

    Update page.tsx

--- app/ARExperience/page.tsx
+++ app/ARExperience/page.tsx
@@ -1,1 +1,15 @@
 
+"use client";
+import { useState } from 'react';
+import { FaUsers, FaEye, FaStar, FaArrowRight } from 'react-icons/fa';
+import Header from '../components/header';
+
+const OurIdentityPage = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default OurIdentityPage;
