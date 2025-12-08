b0d17cd33e739045e6a6d014ffa6a70c082bc849
Yemen-Programer <83095258+Yemen-Programer@users.noreply.github.com>
2025-12-08 19:01:31 +0300

    Update page.tsx

--- app/regions/eastern/page.tsx
+++ app/regions/eastern/page.tsx
@@ -669,7 +669,7 @@ const EasternRegionPage: React.FC = () => {
           <div className="heritage-content">
             <div className="heritage-header">
               <h2>الأماكن التراثية والأثرية</h2>
-              {currentUserId && (
+              { (
                 <div className="wishlist-info">
                   <Link href="/Wishlist" className="wishlist-link">
                     <span className="wishlist-count">
